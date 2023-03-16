d039af40ac548f65b31bf8f64dc37728e3ceb6fe
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-16 17:47:28 -0400

    Create appointmentsRoutes.js

---
+++ controllers/api/appointmentsRoutes.js
@@ -0,0 +1,76 @@
+const router = require("express").Router();
+const { Garden, User } = require("../../models");
+// const withAuth = require('../../utils/auth');
+
+// CREATE a Garden
+router.post("/", async (req, res) => {
+  try {
+    const { title, description } = req.body
+    const newGarden = await Garden.create({
+      title,
+      description,
+      user_id: req.session.user_id
+    });
+    res.redirect('/profile');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET all Appointments
+router.get("/", async (req, res) => {
+  try {
+
+    const appointmentsData = await Appointment.findAll();
+    res.status(200).json(appointmentsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single Appointment
+router.get("/:id", async (req, res) => {
+  try {
+    const appointmentData = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    if (!appointmentData) {
+      res
+        .status(404)
+        .json({ message: "Sorry about that, no records found for this user." });
+
+      return;
+    }
+    res.status(200).json(appointmentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a Appointment
+router.delete("/:id", async (req, res) => {
+  try {
+    const appointmentData = await Appointment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!appointmentData) {
+      res
+        .status(404)
+        .json({ message: "Sorry about that, no records found for this user." });
+
+      return;
+    }
+    const allAppointments = await Appointment.findAll()
+    res.status(200).json(allAppointments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
