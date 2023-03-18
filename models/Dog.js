ad2c77f3a460d47eb8d9ab8701def28f797c37b0
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-18 10:37:13 -0400

    Create Dog.js

---
+++ models/Dog.js
@@ -0,0 +1,74 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dog extends Model { }
+
+Dog.init(
+
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+      
+         birthdate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },   
+
+        breed: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        sex: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        fixed: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        weightInLbs: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+
+        microchip: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+
+        notes: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+      
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'dog',
+    }
+);
+
+module.exports = Dog;
