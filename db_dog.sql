d400bacee87f4ba94dcd3152b6cdfb4f5d312188
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-25 12:40:08 -0400

    Update db_dog.sql

--- db_dog.sql
+++ db_dog.sql
@@ -1,4 +1,58 @@
-CREATE TABLE `db_dog`.`tbl_dog` (`DogId` INT(15) NOT NULL AUTO_INCREMENT , `flddogName` VARCHAR(50) NOT NULL , `flddogDOB` DATE NOT NULL, `flddogAgeInYears` INT (3) NOT NULL , `flddogBreed` VARCHAR(50) NOT NULL , `flddogSex`VARCHAR(10) NOT NULL , `flddogWeightInPounds`, `flddogSterilized`, `flddogMicrochip`, `flddogFood` VARCHAR(50) NOT NULL , `flddogMedication`, `flddogCondition` VARCHAR(50) NOT NULL , PRIMARY KEY (`DogId`)) ENGINE = InnoDB;
+--MYDOGHEALTH COMPLETE LIST OF SQL CODES
+
+
+
+--HANDLER/OWNER INFO SQL
+
+CREATE TABLE `db_handler`.`tbl_handler` (`HandlerId` INT(15) NOT NULL AUTO_INCREMENT , `fldhandlerFirstName` VARCHAR(25) NOT NULL , `fldhandlerLastName` VARCHAR(25) NOT NULL , `fldhandlerEmail` VARCHAR(50) NOT NULL , `fldhandlerPassword` VARCHAR(25) NOT NULL , `fldhandlerPhone` INT(15) NOT NULL , `fldhandlerAddress` VARCHAR(50) NOT NULL , `fldhandlerDog` VARCHAR(50) NOT NULL , `fldhandlerAppointment` VARCHAR(50) NOT NULL , `fldhandlerVeterinarian` VARCHAR(50) NOT NULL , `fldhandlerNotes` VARCHAR(150) NOT NULL , PRIMARY KEY (`HandlerId`)) ENGINE = InnoDB;
+
+SELECT * FROM `tbl_handler` WHERE 1
+
+SELECT `HandlerId`, `fldhandlerFirstName`, `fldhandlerLastName`, `fldhandlerEmail`, `fldhandlerPassword`, `fldhandlerPhone`, `fldhandlerAddress`, `fldhandlerDog`, `fldhandlerAppointment`, `fldhandlerVeterinarian`, `fldhandlerNotes` FROM `tbl_handler` WHERE 1
+
+INSERT INTO `tbl_handler`(`HandlerId`, `fldhandlerFirstName`, `fldhandlerLastName`, `fldhandlerEmail`, `fldhandlerPassword`, `fldhandlerPhone`, `fldhandlerAddress`, `fldhandlerDog`, `fldhandlerAppointment`, `fldhandlerVeterinarian`, `fldhandlerNotes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]','[value-11]')
+
+UPDATE `tbl_handler` SET `HandlerId`='[value-1]',`fldhandlerFirstName`='[value-2]',`fldhandlerLastName`='[value-3]',`fldhandlerEmail`='[value-4]',`fldhandlerPassword`='[value-5]',`fldhandlerPhone`='[value-6]',`fldhandlerAddress`='[value-7]',`fldhandlerDog`='[value-8]',`fldhandlerAppointment`='[value-9]',`fldhandlerVeterinarian`='[value-10]',`fldhandlerNotes`='[value-11]' WHERE 1
+
+DELETE FROM `tbl_handler` WHERE 0
+
+
+
+--VETERINARIAN SQL
+
+CREATE TABLE `db_veterinarian`.`tbl_veterinarian` (`VeterinarianId` INT(15) NOT NULL AUTO_INCREMENT , `fldvetClinic` VARCHAR(50) NOT NULL , `fldvetAddress` VARCHAR(150) NOT NULL , `fldvetPhone` INT(15) NOT NULL , `fldvetEmail` VARCHAR(50) NOT NULL , `fldvetDoctor` VARCHAR(50) NOT NULL , `fldvetNotes` VARCHAR(150) NOT NULL , PRIMARY KEY (`VeterinarianId`)) ENGINE = InnoDB;
+
+SELECT * FROM `tbl_veterinarian` WHERE 1
+
+SELECT `VeterinarianId`, `fldvetClinic`, `fldvetAddress`, `fldvetPhone`, `fldvetEmail`, `fldvetDoctor`, `fldvetDog`, `fldvetNotes` FROM `tbl_veterinarian` WHERE 1
+
+INSERT INTO `tbl_veterinarian`(`VeterinarianId`, `fldvetClinic`, `fldvetAddress`, `fldvetPhone`, `fldvetEmail`, `fldvetDoctor`, `fldvetDog`, `fldvetNotes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]')
+
+UPDATE `tbl_veterinarian` SET `VeterinarianId`='[value-1]',`fldvetClinic`='[value-2]',`fldvetAddress`='[value-3]',`fldvetPhone`='[value-4]',`fldvetEmail`='[value-5]',`fldvetDoctor`='[value-6]',`fldvetDog`='[value-7]',`fldvetNotes`='[value-8]' WHERE 1
+
+DELETE FROM `tbl_veterinarian` WHERE 0
+
+
+
+--APPOINTMENT SQL
+
+CREATE TABLE `db_appointment`.`tbl_appointment` (`AppointmentId` INT(15) NOT NULL AUTO_INCREMENT , `fldappointmentTitle` VARCHAR(50) NOT NULL , `fldappointmentLocation` VARCHAR(150) NOT NULL , `fldappointmentContact` VARCHAR(50) NOT NULL , `fldappointmentTime` DATETIME NOT NULL , `fldappointmentRequiredDocs` VARCHAR(150) NOT NULL , `fldappointmentCost` VARCHAR(50) NOT NULL , `fldappointmentNotes` VARCHAR(150) NOT NULL , PRIMARY KEY (`AppointmentId`)) ENGINE = InnoDB;
+
+SELECT * FROM `tbl_appointment` WHERE 1
+
+SELECT `AppointmentId`, `fldappointmentTitle`, `fldappointmentLocation`, `fldappointmentContact`, `fldappointmentTime`, `fldappointmentRequiredDocs`, `fldappointmentCost`, `fldappointmentNotes` FROM `tbl_appointment` WHERE 1
+
+INSERT INTO `tbl_appointment`(`AppointmentId`, `fldappointmentTitle`, `fldappointmentLocation`, `fldappointmentContact`, `fldappointmentTime`, `fldappointmentRequiredDocs`, `fldappointmentCost`, `fldappointmentNotes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]')
+
+UPDATE `tbl_appointment` SET `AppointmentId`='[value-1]',`fldappointmentTitle`='[value-2]',`fldappointmentLocation`='[value-3]',`fldappointmentContact`='[value-4]',`fldappointmentTime`='[value-5]',`fldappointmentRequiredDocs`='[value-6]',`fldappointmentCost`='[value-7]',`fldappointmentNotes`='[value-8]' WHERE 1
+
+DELETE FROM `tbl_appointment` WHERE 0
+
+
+
+--DOG INFO SQL
+
+CREATE TABLE `db_dog`.`tbl_dog` (`DogId` INT(15) NOT NULL AUTO_INCREMENT , `flddogName` VARCHAR(50) NOT NULL , `flddogDOB` DATE NOT NULL, `flddogAgeInYears` INT (3) NOT NULL , `flddogBreed` VARCHAR(50) NOT NULL , `flddogSex`VARCHAR(10) NOT NULL , `flddogWeightInPounds` INT(5) NOT NULL, `flddogSterilized` VARCHAR(10) NOT NULL , `flddogMicrochip` INT(20) NOT NULL, `flddogFood` VARCHAR(50) NOT NULL , `flddogMedication`, `flddogCondition` VARCHAR(50) NOT NULL , PRIMARY KEY (`DogId`)) ENGINE = InnoDB;
 
 SELECT * FROM `tbl_dog` WHERE 1
 
@@ -12,3 +66,48 @@ DELETE FROM `tbl_dog` WHERE 0
 
 
 
+--MEDICATION SQL
+
+CREATE TABLE `db_medication`.`tbl_medication` (`MedicationId` INT NOT NULL AUTO_INCREMENT , `fldmedicationName` VARCHAR(50) NOT NULL , `fldmedicationStartDate` DATETIME NOT NULL , `fldmedicationEndDate` DATETIME NOT NULL , `fldmedicationDose` VARCHAR(50) NOT NULL , `fldmedicationFrequency` VARCHAR(50) NOT NULL , `fldmedicationMethod` VARCHAR(50) NOT NULL , `fldmedicationDoctor` VARCHAR(50) NOT NULL , `fldmedicationNotes` VARCHAR(150) NOT NULL , PRIMARY KEY (`MedicationId`)) ENGINE = InnoDB;
+
+SELECT * FROM `tbl_medication` WHERE 1
+
+SELECT `MedicationId`, `fldmedicationName`, `fldmedicationStartDate`, `fldmedicationEndDate`, `fldmedicationDose`, `fldmedicationFrequency`, `fldmedicationMethod`, `fldmedicationDoctor`, `fldmedicationNotes` FROM `tbl_medication` WHERE 1
+
+INSERT INTO `tbl_medication`(`MedicationId`, `fldmedicationName`, `fldmedicationStartDate`, `fldmedicationEndDate`, `fldmedicationDose`, `fldmedicationFrequency`, `fldmedicationMethod`, `fldmedicationDoctor`, `fldmedicationNotes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]')
+
+UPDATE `tbl_medication` SET `MedicationId`='[value-1]',`fldmedicationName`='[value-2]',`fldmedicationStartDate`='[value-3]',`fldmedicationEndDate`='[value-4]',`fldmedicationDose`='[value-5]',`fldmedicationFrequency`='[value-6]',`fldmedicationMethod`='[value-7]',`fldmedicationDoctor`='[value-8]',`fldmedicationNotes`='[value-9]' WHERE 1
+
+DELETE FROM `tbl_medication` WHERE 0
+
+
+
+--FOOD SQL
+
+CREATE TABLE `db_food`.`tbl_food` (`FoodId` INT(15) NOT NULL AUTO_INCREMENT , `fldfoodName` VARCHAR(50) NOT NULL , `fldfoodBrand` VARCHAR(50) NOT NULL , `fldfoodAmount` VARCHAR(20) NOT NULL , `fldfoodFrequency` VARCHAR(20) NOT NULL , `fldfoodFavorite` VARCHAR(10) NOT NULL , `fldfoodNotes` VARCHAR(150) NOT NULL , PRIMARY KEY (`FoodId`)) ENGINE = InnoDB;
+
+SELECT * FROM `tbl_food` WHERE 1
+
+SELECT `FoodId`, `fldfoodName`, `fldfoodBrand`, `fldfoodAmount`, `fldfoodFrequency`, `fldfoodFavorite`, `fldfoodNotes` FROM `tbl_food` WHERE 1
+
+INSERT INTO `tbl_food`(`FoodId`, `fldfoodName`, `fldfoodBrand`, `fldfoodAmount`, `fldfoodFrequency`, `fldfoodFavorite`, `fldfoodNotes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')
+
+UPDATE `tbl_food` SET `FoodId`='[value-1]',`fldfoodName`='[value-2]',`fldfoodBrand`='[value-3]',`fldfoodAmount`='[value-4]',`fldfoodFrequency`='[value-5]',`fldfoodFavorite`='[value-6]',`fldfoodNotes`='[value-7]' WHERE 1
+
+DELETE FROM `tbl_food` WHERE 0
+
+
+
+--CONDITION SQL
+
+CREATE TABLE `db_condition`.`tbl_condition` (`ConditionId` INT(15) NOT NULL AUTO_INCREMENT , `fldconditionName` VARCHAR(50) NOT NULL , `fldconditionDateDiagnosed` DATE NOT NULL , `fldconditionDoctor` VARCHAR(50) NOT NULL , `fldconditionTreatment` VARCHAR(150) NOT NULL , `fldconditionPrognosis` VARCHAR(150) NOT NULL , `fldconditionNotes` VARCHAR(150) NOT NULL , PRIMARY KEY (`ConditionId`)) ENGINE = InnoDB;
+
+SELECT * FROM `tbl_condition` WHERE 1
+
+SELECT `ConditionId`, `fldconditionName`, `fldconditionDateDiagnosed`, `fldconditionDoctor`, `fldconditionTreatment`, `fldconditionPrognosis`, `fldconditionNotes` FROM `tbl_condition` WHERE 1
+
+INSERT INTO `tbl_condition`(`ConditionId`, `fldconditionName`, `fldconditionDateDiagnosed`, `fldconditionDoctor`, `fldconditionTreatment`, `fldconditionPrognosis`, `fldconditionNotes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')
+
+UPDATE `tbl_condition` SET `ConditionId`='[value-1]',`fldconditionName`='[value-2]',`fldconditionDateDiagnosed`='[value-3]',`fldconditionDoctor`='[value-4]',`fldconditionTreatment`='[value-5]',`fldconditionPrognosis`='[value-6]',`fldconditionNotes`='[value-7]' WHERE 1
+
+DELETE FROM `tbl_condition` WHERE 0
