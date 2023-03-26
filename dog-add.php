9ab8ae38b0015a9bd5aef67ab4492fc8a2c1db6e
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-26 14:00:01 -0400

    Update dog-add.php

--- dog-add.php
+++ dog-add.php
@@ -4,8 +4,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Add Dog</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-SSlL99sDpE+1sG+nCoyv/QOFeDnX9zPbZpHtWkEZtRgFtWTwGhJq3rW2ptjQYafX9xlNcQ8zLWzs82vAVNUnyA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="style.css">
 </head>
 ​
@@ -38,27 +39,13 @@
     <div class="container text-center">
       <div class="row">
         <div class="col-lg-12">
-          <h6>Dog Information</h6>
+          <h6>My Dog Information</h6>
         </div>
       </div>
     </div>
   </section>
-​
-<section class="">
-  <div class="container text-center">
-    <div class="row">
-      <div class="col-lg-12">
-<table class="table">
-  <tbody>
-    <?php include 'dog-read.php'; ?>
-  </tbody>
-</table>
-</div>
-</div>
-</div>
-</section>
-
-  <section>
+ 
+ <section>
   <div class="container">
     <div class="row">
         <div class="col-lg-12">
@@ -66,7 +53,7 @@
               <div class="text-center">
 <fieldset>
 <legend>Add Dog</legend>
-<form name="frmDog" method="post" action="add-dog.php">
+<form name="frmDog" method="post" action="dog-create.php">
 
 <p>
 <label for="flddogName">Dog Name</label>
@@ -109,10 +96,41 @@
 </div>
 </div>
 </section>
+
+<section>
+  <div class="row m-0">
+    <div class="col-lg-3 bottom-btn">
+        <a href="Welcome Screen With no saved dogs.html">
+          <div class="text-center">
+            <img src="images/pawprinticon.png" class="rounded pt-3" alt="...">
+          </div>
+        </a>
+    </div>
+    <div class="col-lg-3 bottom-btn">
+      <a href="Appointment Schedule.html">
+        <div class="text-center">
+          <img src="images/appointment.png" class="rounded" alt="...">
+        </div>
+      </a>
+  </div>
+  <div class="col-lg-3 bottom-btn">
+    <a href="Veterinarian Information.html">
+      <div class="text-center">
+        <img src="images/veterinarian.png" class="rounded" alt="...">
+      </div>
+    </a>
+</div>
+<div class="col-lg-3 bottom-btn">
+  <a href="Resources.html">
+    <div class="text-center">
+      <img src="images/add.png" class="rounded" alt="...">
+    </div>
+  </a>
+</div>
+  </div>
+</section>
 ​
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
-</html>
-​
 </html>
