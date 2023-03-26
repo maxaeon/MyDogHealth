c425ef64f0e57b75f6592c2842ec511ca0d4b0b6
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-26 13:08:42 -0400

    Update dog-update.php

---
+++ dog-update.php
@@ -0,0 +1,18 @@
+<?php
+  include 'db.php';
+  $DogId = $_POST['DogId'];
+  $txtdogName = $_POST['flddogName'];
+  $txtdogDOB = $_POST['flddogDOB'];
+  $txtdogAgeInYears = $_POST['flddogAgeInYears'];
+  $txtdogBreed = $_POST['flddogBreed'];
+  $txtdogSex = $_POST['flddogSex'];
+  $txtdogWeightInPounds = $_POST['flddogWeightInPounds'];
+  $txtdogSterilized = $_POST['flddogSterilized'];
+  $txtdogMicrochip = $_POST['flddogMicrochip'];
+  $txtdogNotes = $_POST['flddogNotes'];
+
+  $sql = "update tbl_dog set flddogName='$txtdogName', flddogDOB='$txtdogDOB', flddogAgeInYears= '$txtdogAgeInYears', flddogBreed= '$txtdogBreed', flddogSex= '$txtdogSex', flddogWeightInPounds= '$txtdogWeightInPounds', flddogSterilized= '$txtdogSterilized', flddogMicrochip= '$txtdogMicrochip', flddogNotes= '$txtdogNotes' where DogId=$DogId";
+  $result = $conn->query($sql);
+  $conn->close();
+  //header("location: index.php");
+?>
