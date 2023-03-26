6b031a2e29d2765701fbdd071aec94e97645c1cc
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-26 15:16:28 -0400

    Add files via upload

---
+++ vet-create.php
@@ -0,0 +1,24 @@
+<?php
+// database connection code
+ $con = mysqli_connect('localhost', 'myUser', 'myPasswd','db_dog');
+// get the post records
+$txtvetClinic = $_POST['txtvetClinic'];
+$txtvetAddress = $_POST['txtvetAddress'];
+$txtvetPhone = $_POST['txtvetPhone'];
+$txtvetEmail = $_POST['txtvetEmail'];
+$txtvetDoctor = $_POST['txtvetDoctor'];
+$txtvetDog = $_POST['txtvetDog'];
+$txtvetNotes = $_POST['txtvetNotes'];
+
+echo "Vet Record Prior to Insertion";
+// database insert SQL code
+$sql = "INSERT INTO tbl_dog (fldvetClinic, fldvetAddress, fldvetPhone, fldvetEmail, fldvetDoctor, fldvetDog, fldvetNotes) VALUES ('$txtvetClinic','$txtvetAddress', '$txtvetPhone', '$txtvetEmail', '$txtvetDoctor', '$txtvetDog', '$txtvetNotes' )";
+echo $sql;
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Vet Record Updated Successfully";
+}
+?>
