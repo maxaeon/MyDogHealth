5c3944ad0227ce72fdf2977244b5a462f6fdd750
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 18:26:34 -0400

    Add files via upload

---
+++ welcome-read.php
@@ -0,0 +1,21 @@
+<?php
+// database connection code
+include 'db.php';
+  $sql = "select * from tbl_dog";
+  $result = $conn->query($sql);
+  while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+
+      echo "<td> Dog Name:</td>";
+        echo "<td>" . $row['flddogName'] . "</td>";
+        echo "</tr>";
+        
+   // echo '<td><a class="btn btn-primary" href="index.php?id=' . $row['DogId'] . '" role="button">Update</a></td>';
+      }
+    //echo '<td><a class="btn btn-danger" href="dog-delete.php?id=' . $row['DogId'] . '" role="button">Delete</a></td>';
+
+  
+
+  
+  $conn->close();
+?>
