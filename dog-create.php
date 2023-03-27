8f8b2ac169d0d415dbf914ffb1b5a69fcb3f2b35
fsheikumich <fsheik@umich.edu>
2023-03-26 22:39:38 -0400

    Dog create

--- dog-create.php
+++ dog-create.php
@@ -14,16 +14,16 @@ $txtdogMicrochip = $_POST['txtdogMicrochip'];
 $txtdogNotes = $_POST['txtdogNotes'];
 
 
-echo "Dog Record before Successfully";
+//echo "Dog Record before Successfully";
 // database insert SQL code
-$sql = "INSERT INTO tbl_dog (flddogName, flddogDOB, flddogAgeInYears, flddogBreed, fldDogSex, flddogWeightInPounds, flddogSterilized, flddogMicrochip, flddogNotes) VALUES ('$txtdogName', '$txtdogDOB', '$txtdogAgeInYears', '$txtdogBreed', '$txtdogSex', '$txtdogWeightInPounds', '$txtdogSterilized', '$txtdogMicrochip', '$txtdogNotes')";
-echo $sql;
+$sql = "INSERT INTO tbl_dog (flddogHandler, flddogName, flddogDOB, flddogAgeInYears, flddogBreed, fldDogSex, flddogWeightInPounds, flddogSterilized, flddogMicrochip, flddogNotes) VALUES (' ', '$txtdogName', '$txtdogDOB', '$txtdogAgeInYears', '$txtdogBreed', '$txtdogSex', '$txtdogWeightInPounds', '$txtdogSterilized', '$txtdogMicrochip', '$txtdogNotes')";
+//echo $sql;
 // insert in database 
 $rs = mysqli_query($con, $sql);
 
 if($rs)
 {
-	echo "Dog Record Updated Successfully";
+	include("dog-view.php");
 }
 
 
