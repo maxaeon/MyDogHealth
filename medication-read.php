3ed97ea1817c3eab5f2f2fea80ffb61a8fbb1f00
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 19:11:08 -0400

    Update medication-read.php

--- medication-read.php
+++ medication-read.php
@@ -6,18 +6,30 @@ include 'db.php';
   while($row = $result->fetch_assoc()) {
 
 
+    echo "</tr>";
+    echo "<td> Medication Name:</td>";
     echo "<td>" . $row['fldmedicationName'] . "</td>";
+    echo "</tr>";
+    echo "<td> Start Date:</td>";
     echo "<td>" . $row['fldmedicationStartDate'] . "</td>";
+    echo "</tr>";
+    echo "<td> End Date:</td>";
     echo "<td>" . $row['fldmedicationEndDate'] . "</td>";
+    echo "</tr>";
+    echo "<td> Dose:</td>";
     echo "<td>" . $row['fldmedicationDose'] . "</td>";
+    echo "</tr>";
+    echo "<td> Frequency:</td>";
     echo "<td>" . $row['fldmedicationFrequency'] . "</td>";
+    echo "</tr>";
+    echo "<td> Administration Method:</td>";
     echo "<td>" . $row['fldmedicationMethod'] . "</td>";
+    echo "</tr>";
+    echo "<td> Doctor(s):</td>";
     echo "<td>" . $row['fldmedicationDoctor'] . "</td>";
+    echo "</tr>";
+    echo "<td> Notes:</td>";
     echo "<td>" . $row['fldmedicationNotes'] . "</td>";
-
-    echo '<td><a class="btn btn-primary" href="" role="button">Update</a></td>';  
-    echo '<td><a class="btn btn-danger" href="" role="button">Delete</a></td>';
-
     
     echo "</tr>";
   }
