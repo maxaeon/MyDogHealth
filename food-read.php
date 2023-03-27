494a48d2493c8e5b0fdbca361b2cfb6c0d6a4028
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 13:39:50 -0400

    Update food-read.php

--- food-read.php
+++ food-read.php
@@ -13,6 +13,8 @@ include 'db.php';
     echo "<td>" . $row['fldfoodFavorite'] . "</td>";
     echo "<td>" . $row['fldfoodNotes'] . "</td>";
 
+    echo '<td><a class="btn btn-primary" href="" role="button">Update</a></td>';  
+    echo '<td><a class="btn btn-danger" href="" role="button">Delete</a></td>';
 
 
    
