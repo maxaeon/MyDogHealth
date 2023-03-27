721407af89e43e5930363614c7cbb602090120b7
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 13:19:50 -0400

    Update handler-create.php

--- handler-create.php
+++ handler-create.php
@@ -9,17 +9,9 @@ $txthandlerPassword = $_POST['txthandlerPassword'];
 $txthandlerPhone = $_POST['txthandlerPhone'];
 $txthandlerAddress = $_POST['txthandlerAddress'];
 
-echo "Record to Insert";
+
 // database insert SQL code
 $sql = "INSERT INTO tbl_handler (fldhandlerFirstName, fldhandlerLastName, fldhandlerEmail, fldhandlerPassword, fldhandlerPhone, fldhandlerAddress) VALUES ( '$txthandlerFirstName', '$txthandlerLastName', '$txthandlerEmail', '$txthandlerPassword', '$txthandlerPhone', '$txthandlerAddress')";
-echo $sql;
-// insert in database 
-$rs = mysqli_query($con, $sql);
-
-if($rs)
-{
-	echo "Record Updated Successfully";
-}
 
 
 $rs = mysqli_query($con, $sql);
