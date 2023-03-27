6b9c2ae15af89aa8d42b3ecd29c1f812ef41c3ff
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 19:14:16 -0400

    Update vet-read.php

--- vet-read.php
+++ vet-read.php
@@ -6,18 +6,27 @@ include 'db.php';
   while($row = $result->fetch_assoc()) {
 
 
+    echo "</tr>";
+    echo "<td> Clinic Name:</td>";
     echo "<td>" . $row['fldvetClinic'] . "</td>";
+    echo "</tr>";
+    echo "<td> Address:</td>";
     echo "<td>" . $row['fldvetAddress'] . "</td>";
+    echo "</tr>";
+    echo "<td> Phone:</td>";
     echo "<td>" . $row['fldvetPhone'] . "</td>";
+    echo "</tr>";
+    echo "<td> Email:</td>";
     echo "<td>" . $row['fldvetEmail'] . "</td>";
+    echo "</tr>";
+    echo "<td> Doctor:</td>";
     echo "<td>" . $row['fldvetDoctor'] . "</td>";
+    echo "</tr>";
+    echo "<td> Dog(s) in their Care:</td>";
     echo "<td>" . $row['fldvetDog'] . "</td>";
+    echo "</tr>";
+    echo "<td> Notes:</td>";
     echo "<td>" . $row['fldvetNotes'] . "</td>";
-
-    echo '<td><a class="btn btn-primary" href="" role="button">Update</a></td>';  
-    echo '<td><a class="btn btn-danger" href="" role="button">Delete</a></td>';
-
-    
     echo "</tr>";
 
   }
