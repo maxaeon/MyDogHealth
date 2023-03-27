e41c7512dc2e3e19d5d1270770902fcc92908416
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 19:00:33 -0400

    Update food-read.php

--- food-read.php
+++ food-read.php
@@ -6,16 +6,25 @@ include 'db.php';
   while($row = $result->fetch_assoc()) {
 
 
+
+    echo "</tr>";
+    echo "<td> Food Name:</td>";
     echo "<td>" . $row['fldfoodName'] . "</td>";
+    echo "</tr>";
+    echo "<td> Brand:</td>";
     echo "<td>" . $row['fldfoodBrand'] . "</td>";
+    echo "</tr>";
+    echo "<td> Amount:</td>";
     echo "<td>" . $row['fldfoodAmount'] . "</td>";
+    echo "</tr>";
+    echo "<td> Frequency:</td>";
     echo "<td>" . $row['fldfoodFrequency'] . "</td>";
+    echo "</tr>";
+    echo "<td> Is this food a favorite?</td>";
     echo "<td>" . $row['fldfoodFavorite'] . "</td>";
+    echo "</tr>";
+    echo "<td> Notes:</td>";
     echo "<td>" . $row['fldfoodNotes'] . "</td>";
-
-    echo '<td><a class="btn btn-primary" href="" role="button">Update</a></td>';  
-    echo '<td><a class="btn btn-danger" href="" role="button">Delete</a></td>';
-
 
    
     echo "</tr>";
