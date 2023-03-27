85f5aa2f9b4ad4b750c0a03b9eeccbd7493548c3
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2023-03-27 18:35:24 -0400

    Update appointment-read.php

--- appointment-read.php
+++ appointment-read.php
@@ -6,17 +6,29 @@ include 'db.php';
   while($row = $result->fetch_assoc()) {
 
 
+    echo "</tr>";
+    echo "<td> Appointment Title:</td>";
     echo "<td>" . $row['fldappointmentTitle'] . "</td>";
+    echo "</tr>";
+    echo "<td>  Location:</td>";
     echo "<td>" . $row['fldappointmentLocation'] . "</td>";
+    echo "</tr>";
+    echo "<td> Contact :</td>";
     echo "<td>" . $row['fldappointmentContact'] . "</td>";
+    echo "</tr>";
+    echo "<td> Time:</td>";
     echo "<td>" . $row['fldappointmentTime'] . "</td>";
+    echo "</tr>";
+    echo "<td> Required Documents and Materials:</td>";
     echo "<td>" . $row['fldappointmentRequiredDocs'] . "</td>";
+    echo "</tr>";
+    echo "<td> Cost:</td>";
     echo "<td>" . $row['fldappointmentCost'] . "</td>";
+    echo "</tr>";
+    echo "<td> Notes:</td>";
     echo "<td>" . $row['fldappointmentNotes'] . "</td>";
 
 
-   echo '<td><a class="btn btn-primary" href="" role="button">Update</a></td>';  
-  echo '<td><a class="btn btn-danger" href="" role="button">Delete</a></td>';
     
     echo "</tr>";
   }
